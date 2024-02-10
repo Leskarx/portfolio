4b3417c60b485bf94588fc58e89431ca6edc4c68
Leskarx <leskarzone123@gmail.com>
2024-02-10 22:30:55 +0530

    added some more information

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "High school",
+    location: "Dhemaji",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "bhai ki likhu najanu",
     icon: React.createElement(LuGraduationCap),
     date: "2019",
   },
   {
     title: "Front-End Developer",
-    location: "Orlando, FL",
+    location: "USA",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as a front-end developer -sapna hai",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2026-2027",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "INdia",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2027 - present",
   },
 ] as const;
 
