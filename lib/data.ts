191a896b528404ca245203bd330e7ca727660487
LeskarX <leskarzone123@gmail.com>
2025-03-25 08:38:23 +0530

    updated project

--- lib/data.ts
+++ lib/data.ts
@@ -1,11 +1,11 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
+// import { LuGraduationCap } from "react-icons/lu";
 import easyrent from "@/public/easyRent.png";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import youtubeS from "@/public/youtubeS.png";
+import projectBhasha from "@/public/projectBhasha.png";
+// import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -35,59 +35,59 @@ export const links = [
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "High school",
-    location: "Dhemaji",
-    description:
-      "bhai ki likhu najanu",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "USA",
-    description:
-      "I worked as a front-end developer -sapna hai",
-    icon: React.createElement(CgWorkAlt),
-    date: "2026-2027",
-  },
+  // {
+  //   title: "",
+  //   location: "Dhemaji",
+  //   description:
+  //     "bhai ki likhu najanu",
+  //   icon: React.createElement(LuGraduationCap),
+  //   date: "2019",
+  // },
   {
     title: "Full-Stack Developer",
-    location: "INdia",
+    location: "Subspace",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2027 - present",
+      "I worked as a full-stack developer at Subspace, a startup that provides a platform for remote teams to collaborate and communicate. I worked on the frontend using React, Next.js, and Tailwind, and on the backend using Prisma and MongoDB.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2024-2025",
   },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "INdia",
+  //   description:
+  //     "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2027 - present",
+  // },
 ] as const;
 
 export const projectsData = [
   {
     title: "Easy Rent",
     github:"https://github.com/Leskarx/Easyrent",
-    live:"https://www.theease.rent/",
+    live:"https://easyrentt.vercel.app/",
     description:
       "Easy Rent is a comprehensive platform for finding and managing rental properties, including PG accommodations and rooms.",
     tags: [ "Next.js", "MongoDB", "Tailwind", "Prisma"],
     imageUrl: easyrent,
   },
   {
-    title: "rmtDev",
-    github:"https://github.com/Leskarx/Easyrent",
-    live:"https://www.theease.rent/",
+    title: "Project Bhasha",
+    github:"https://github.com/Leskarx/projectBhasha",
+    live:"https://github.com/Leskarx/projectBhasha",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "An AI-powered website extracts text from images and PDFs, translating it into regional languages for accessibility.",
+    tags: ["NLP", "Python", "openCV", "Node.js", "Express"],
+    imageUrl: projectBhasha,
   },
   {
-    title: "Word Analytics",
-    github:"https://github.com/Leskarx/Easyrent",
-    live:"https://www.theease.rent/",
+    title: "Youtube Summarizer",
+    github:"https://github.com/Leskarx/YouTube-Video-Summarizer",
+    live:"https://youtube-summarizer-leskar.vercel.app/",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "An AI-powered website summarizes YouTube videos from links, allowing users to log in, generate summaries, and view history.",
+    tags: ["N8N", "React.js", "Node.js", "Tailwind", "Node.js"],
+    imageUrl: youtubeS,
   },
 ] as const;
 
