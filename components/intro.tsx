87e8dc72ee85a7d40e6671b7561fbc4f934e122f
LeskarX <leskarzone123@gmail.com>
2025-08-11 17:21:04 +0530

    changed image border

--- components/intro.tsx
+++ components/intro.tsx
@@ -37,7 +37,7 @@ export default function Intro() {
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-24 w-24 rounded-full object-cover shadow-xl"
             />
           </motion.div>
 
