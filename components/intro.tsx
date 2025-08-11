68113e04bfc1f52184877d467bb64da21ba45b23
LeskarX <leskarzone123@gmail.com>
2025-08-11 16:56:29 +0530

    changed Footer

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,8 +65,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Gouri shankar.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">1 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        building <span className="italic">sites & apps.</span>
       </motion.h1>
 
       <motion.div
