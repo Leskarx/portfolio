45c79ae64aef5784ae1979d99206fa00ebadbb94
LeskarX <leskarzone123@gmail.com>
2025-08-11 17:14:40 +0530

    added updated Resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -90,10 +90,10 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Resume.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
